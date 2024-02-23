8885318092f731876f86512c648a8f04ceb38b6b
Kavya Patel <ayvakbackwards@gmail.com>
2024-02-23 09:54:20 +0530

    Adding-Notes-View

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,6 +1,5 @@
-import 'package:noter/views/verify_email_view.dart';
-
 const loginRoute = '/login/';
 const registerRoute = '/register/';
 const notesRoute = '/notes/';
 const verifyEmailRoute = '/verify-email/';
+const newNoteRoute = '/notes/new-note/';
--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:noter/constants/routes.dart';
 import 'package:noter/services/auth/auth_service.dart';
 import 'package:noter/views/login_view.dart';
-import 'package:noter/views/notes_view.dart';
+import 'package:noter/views/notes/new_note_view.dart';
+import 'package:noter/views/notes/notes_view.dart';
 import 'package:noter/views/register_view.dart';
 import 'package:noter/views/verify_email_view.dart';
 
@@ -20,6 +21,7 @@ void main() {
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
+      newNoteRoute: (context) => const NewNoteView(),
     },
   ));
 }
