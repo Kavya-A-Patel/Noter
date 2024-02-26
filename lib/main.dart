d75caa95dfd84813a306a7e60641db78727433e0
Kavya Patel <ayvakbackwards@gmail.com>
2024-02-26 09:59:52 +0530

    Making-it-so-that-we-can-now-update-notes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:noter/constants/routes.dart';
 import 'package:noter/services/auth/auth_service.dart';
 import 'package:noter/views/login_view.dart';
-import 'package:noter/views/notes/new_note_view.dart';
+import 'package:noter/views/notes/create_update_note_view.dart';
 import 'package:noter/views/notes/notes_view.dart';
 import 'package:noter/views/register_view.dart';
 import 'package:noter/views/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
       registerRoute: (context) => const RegisterView(),
       notesRoute: (context) => const NotesView(),
       verifyEmailRoute: (context) => const VerifyEmailView(),
-      newNoteRoute: (context) => const NewNoteView(),
+      createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
     },
   ));
 }
